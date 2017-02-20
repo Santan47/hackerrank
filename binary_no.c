7ff55267cfa986cadb92364755a71467262b8df9
santan kumar sharma <santankumarsharma47@gmail.com>
2017-02-20 23:23:45 +0530

    hackerrank problem

---
+++ binary_no.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int x;
+int y;
+int i;
+int bin_array[31];
+int b;
+int blockcount;
+int maxcount;
+int mincount;
+if(bin_array[y] == 1)
+   {
+    blockcount++
+   }
+   else 
+    {
+     ???
+     }
+
